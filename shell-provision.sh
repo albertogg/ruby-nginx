ca11b52389c387940a6096da6e204e48520847a3
albertogg <albertogrespan@gmail.com>
2013-12-27 13:14:31 -0430

    Script para actualizar ubuntu.

--- shell-provision.sh
+++ shell-provision.sh
@@ -1,7 +1,14 @@
 #! /usr/bin/env sh
 
+sudo -s
 # update packages
-sudo apt-get update -y
+apt-get update -y
+apt-get update -y --fix-missing
 
 # upgrade installed packages
-sudo apt-get upgrade -y
+apt-get upgrade -y
+
+apt-get install -y linux-headers-generic-lts-raring linux-image-generic-lts-raring python-software-properties
+add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+add-apt-repository "deb http://ppa.launchpad.net/dns/gnu/ubuntu $(lsb_release -sc) main"
+apt-get update -y
