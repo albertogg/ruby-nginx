6265482469e72de1bb0ef93c4812d3a055afd36c
albertogg <albertogrespan@gmail.com>
2014-01-15 00:53:53 -0430

    Prueba para provision de vagrant.

--- shell-provision.sh
+++ shell-provision.sh
@@ -1,14 +1,15 @@
 #! /usr/bin/env sh
 
 sudo -s
+
+export DEBIAN_FRONTEND=noninteractive
+
 # update packages
 apt-get update -y
 apt-get update -y --fix-missing
+apt-get -o Dpkg::Options::="--force-confnew" -o Dpkg::Options::="--force-confold" --force-yes -fuy upgrade
 
-# upgrade installed packages
-apt-get upgrade -y
-
-apt-get install -y linux-headers-generic-lts-raring linux-image-generic-lts-raring python-software-properties
+apt-get install -y python-software-properties
 add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 add-apt-repository "deb http://ppa.launchpad.net/dns/gnu/ubuntu $(lsb_release -sc) main"
 apt-get update -y
