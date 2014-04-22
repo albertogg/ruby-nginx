a64c6d97208224358f7916371522680f0f0a7cb0
albertogg <albertogrespan@gmail.com>
2014-04-21 23:29:15 -0430

    Simpler shell-provision

--- shell-provision.sh
+++ shell-provision.sh
@@ -5,11 +5,9 @@ sudo -s
 export DEBIAN_FRONTEND=noninteractive
 
 # update packages
-apt-get update -y
 apt-get update -y --fix-missing
 apt-get -o Dpkg::Options::="--force-confnew" -o Dpkg::Options::="--force-confold" --force-yes -fuy upgrade
 
-apt-get install -y python-software-properties
-add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
-add-apt-repository "deb http://ppa.launchpad.net/dns/gnu/ubuntu $(lsb_release -sc) main"
+echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe" > /etc/apt/sources.list
 apt-get update -y
+apt-get clean
