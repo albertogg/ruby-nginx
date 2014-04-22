49ffd4ff111452c66fadcff2566b694ea4dec5ca
albertogg <albertogrespan@gmail.com>
2014-04-22 14:19:45 -0430

    Trim the fat

--- ruby/cron-script.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env sh
-
-0 0 * * * sh /var/www/cron/build-script.sh
--- ruby/startup-script.sh
+++
@@ -1,5 +0,0 @@
-# Start cron in the background.
-cron -f &
-
-# start ssh daemon in the background.
-/usr/sbin/sshd -D
